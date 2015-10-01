44b6c21c96323b2dc0ef7f9a9af5109b6d59e43d
sduong <duongseven@gmail.com>
2015-09-30 17:41:04 -0700

    Basic view now returns minimal HTMl

--- functional_test.py
+++ functional_test.py
@@ -3,12 +3,14 @@ from selenium import webdriver
 try: 
     browser = webdriver.Firefox()
     browser.get('http://localhost:8000')
+    assert 'To-Do' in browser.title, "Browser title is:" + browser.title
 except Exception as ex:
     print("error: %s",ex)
 else: 
     print('ok')
 finally:
-    assert 'To-Do' in browser.title, "Browser title is:" + browser.title
+    browser.quit()
+
 
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,19 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
-
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_html(self):
+        request = HttpRequest()         # http request made by the user
+        response = home_page(request)   # htpp response send by the view it returns a HttpResponse object
+        self.assertTrue(response.content.startswith(b'<html>'))  # response.content is html page 
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
 
