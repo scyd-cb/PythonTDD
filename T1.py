91f2f803f09431e371e953e9f300ae47b4cde963
sduong <duongseven@gmail.com>
2015-10-01 17:18:11 -0700

    Front page HTML now generated from template

--- T1.py
+++ T1.py
@@ -35,9 +35,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # check updated list
-        table = self.browse.find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
 
 
          
